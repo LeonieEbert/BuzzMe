4548c92e8f995a09e7617eaa0b9189b36ec3c249
LeonieEbert <ebert.leonie@gmail.com>
2018-03-14 18:14:54 +0100

    bottom navbar

--- app/src/main/java/com/example/buzzme/MainActivity.java
+++ app/src/main/java/com/example/buzzme/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.buzzme;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_active_project:
+                        Toast.makeText(MainActivity.this, "Action Active Project", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_inactive_project:
+                        Toast.makeText(MainActivity.this, "Action Inactive Project", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_overview_project:
+                        Toast.makeText(MainActivity.this, "Action Overview Project", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
