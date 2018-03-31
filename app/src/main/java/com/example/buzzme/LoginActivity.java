85461ea6371c24678cd03a232f13709a0938f95f
AnneTraulsen <s_traulsen@stud.hwr-berlin.de>
2018-03-31 16:57:40 +0200

    Splash screen implemented (timer); outdated compile > implementation; minor changes in strings.xml (--> no hardcoded strings)

--- app/src/main/java/com/example/buzzme/LoginActivity.java
+++ app/src/main/java/com/example/buzzme/LoginActivity.java
@@ -16,33 +16,32 @@ package com.example.buzzme;
  */
 
 
+        //import android.content.Intent;
         import android.os.Bundle;
-        import android.support.annotation.NonNull;
+        //import android.support.annotation.NonNull;
         import android.support.v7.app.AppCompatActivity;
-        import android.text.TextUtils;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
+        //import android.text.TextUtils;
+        //import android.util.Log;
+        //import android.view.View;
+        //import android.widget.EditText;
+        //import android.widget.TextView;
+        //import android.widget.Toast;
 
-        import com.google.android.gms.tasks.OnCompleteListener;
-        import com.google.android.gms.tasks.Task;
+        //import com.google.android.gms.tasks.OnCompleteListener;
+        //import com.google.android.gms.tasks.Task;
         //import com.google.firebase.auth.AuthResult;
         //import com.google.firebase.auth.FirebaseAuth;
         //import com.google.firebase.auth.FirebaseUser;
-        import android.app.ProgressDialog;
+        //import android.app.ProgressDialog;
 
 
 public class LoginActivity extends AppCompatActivity
         //implements View.OnClickListener
         {
 
-        public void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_login);
-
-
 
 
     /*private static final String TAG = "Login";
---
+++ app/src/main/java/com/example/buzzme/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.buzzme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by atrau on 31.03.2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        // set delay to show Logo
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }, 4000);
+            }
+        }
+
