4c9c566bffb4b4215e2e7a8de2b102fcc5be6d76
AnneTraulsen <s_traulsen@stud.hwr-berlin.de>
2018-04-19 23:21:12 +0200

    ProjectStatus attribute inserted to class project, projectAdapter angepasst for overview

--- app/src/main/java/com/example/buzzme/ProjectAdapter.java
+++ app/src/main/java/com/example/buzzme/ProjectAdapter.java
@@ -3,12 +3,10 @@ package com.example.buzzme;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -37,9 +35,7 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
     Project project = projectList.get(position);
     holder.textViewTitle.setText(project.getProjectName());
-    holder.textViewDesc.setText("TEST");
-    holder.textViewRating.setText("TEST");
-    holder.textViewPrice.setText("TEST");
+    holder.textViewTime.setText("3T 5H 31M");
 
     holder.btnEdit.setBackgroundColor(project.getProjectColor());
 
@@ -53,15 +49,13 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
     class ProjectViewHolder extends RecyclerView.ViewHolder{
 
         Button btnEdit;
-        TextView textViewTitle,textViewDesc,textViewRating,textViewPrice;
+        TextView textViewTitle,textViewTime;
 
         public ProjectViewHolder(View itemView) {
             super(itemView);
             btnEdit = itemView.findViewById(R.id.btnEdit);
             textViewTitle = itemView.findViewById(R.id.textViewTitle);
-            textViewDesc= itemView.findViewById(R.id.textViewShortDesc);
-            textViewRating= itemView.findViewById(R.id.textViewRating);
-            textViewPrice= itemView.findViewById(R.id.textViewPrice);
+            textViewTime = itemView.findViewById(R.id.textViewTime);
         }
     }
 }
